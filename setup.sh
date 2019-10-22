75fb2496f5727e9991ca25b7d8d76766ec710ab9
Lucie Hutchins <LucyHut@users.noreply.github.com>
2019-10-22 12:57:25 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd $0
+if [ ! -f ../config/biocore.cfg ]
+then
+   echo "ERROR: ../config/biocore.cfg not found "
+   exit 1
+fi
+if [ ! -f ../config/user.cfg ]
+then
+   echo "ERROR: ../config/user.cfg not found "
+   exit 1
+fi
+
+source ../config/biocore.cfg
+source ../config/user.cfg
+USER_BASE=$MINOTA_BASE/$WS_USER_NAME
+[ ! -d $USER_BASE ] && mkdir $USER_BASE
+
+exit 0
+
