07b3a84ad289d319e65ec36d9b92e4b2442075a9
Lucie Hutchins <LucyHut@users.noreply.github.com>
2019-10-22 12:46:32 -0400

    Update run_single_reads.sh

--- trinity/run_single_reads.sh
+++ trinity/run_single_reads.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 cd $0
+trinity=`which Trinity`
+if [ ! -f $trinity ]
+then
+   echo "ERROR: Trinity not found on PATH - try typing the command 'which Trinity'"
+   exit 1
+fi
 if [ ! -f ../config/single_reads_trinity.cfg ]
 then
    echo "ERROR: ../config/single_reads_trinity.cfg not found "
    exit 1
 fi
 source ../config/single_reads_trinity.cfg
+
+$trinity $CMD_OPTION $READS $CPU_MEM_OPTION
 
 env
 
